1148a4e8de000f328a37627975271f2b9cc97d0f
Paul ROUX <paul.roux03@etud.univ-paris8.fr>
2026-04-01 17:59:23 +0200

    added board methods

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,6 @@
 #include "game_variables.hpp"
 #include "pieces.hpp"
+#include <vector>
 
 class Cell {
 	Piece* piece;
@@ -12,5 +13,31 @@ class Cell {
 class Board {
 	Cell cells[BOARD_W][BOARD_H];
 	public:
-		Cell* findCell(Piece* piece);
+		Cell* findCell(Piece* piece) {
+			for(int i = 0; i < BOARD_W; ++i) {
+				for(int j = 0; j < BOARD_H; ++j) {
+				if(this->cells[BOARD_W][BOARD_H].getPiece() == piece)
+					return &this->cells[BOARD_W][BOARD_H];
+				}
+			}
+			return nullptr;
+		}
+
+		std::vector<Piece*> getPiecesFromTeam(char team) {
+			std::vector<Piece*> res;
+			for(int i = 0; i < BOARD_W; ++i) {
+				for(int j = 0; i < BOARD_H; ++j) {
+					Piece* p = this->cells[i][j].getPiece();
+					if(p->getTeam() == team) {
+						res.push_back(this->cells[i][j].getPiece());
+					}
+				}
+			}
+			return res;
+		}
+
+		int manhattanDist(Cell c1, Cell c2) {
+			int res = (c1.row - c2.row + c1.col - c1.col);
+			return res < 0 ? -res : res;
+		}
 };
