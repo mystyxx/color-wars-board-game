b719381e951d088772a6feb9b52db955a011276e
Paul ROUX <paul.roux03@etud.univ-paris8.fr>
2026-04-01 18:00:40 +0200

    added more game vars

--- src/game_variables.hpp
+++ src/game_variables.hpp
@@ -1,2 +1,12 @@
+#ifndef DEF
+
+#define DEF 1
 #define BOARD_W 20
 #define BOARD_H 20
+
+#define PIECE_COST_CASTLE 15
+#define PIECE_COST_LORD 10
+#define PIECE_COST_WARRIOR 10
+#define PIECE_COST_FARMER 20
+
+#endif
